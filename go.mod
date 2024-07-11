b0f2a0f495b960f4914606f1491226a2cff3c067
cbharshit <harshitchennakadan@gmail.com>
2024-07-11 19:33:20 -0400

    Harshit commit

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/sojoudian/SimpleAPIServer
+module github.com/cbharshit/week11_assignment_group_D
 
 go 1.22.3
--- main.go
+++ main.go
@@ -13,9 +13,14 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome to Home!")
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to About page.")
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/about", aboutHandler)
 
 	log.Println("Starting server on :3001")
 	err := http.ListenAndServe(":3001", mux)
